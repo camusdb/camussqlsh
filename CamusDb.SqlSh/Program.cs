306e205d974b32acfdec4c5355f2ba41be3b1482
Andres Gutierrez <gutierrezandresfelipe@gmail.com>
2024-01-04 16:57:54 -0500

    Dynamic render of queries, support for non queries

--- CamusDb.SqlSh/Program.cs
+++ CamusDb.SqlSh/Program.cs
@@ -1,10 +1,11 @@
 ﻿
 using CamusDB.Client;
 using Spectre.Console;
+using System.Diagnostics;
 
 Console.WriteLine("CamusDB 0.0.1");
 
-CamusConnection connection = await GetConnection();
+(CamusConnection connection, CamusConnectionStringBuilder builder) = await GetConnection();
 
 
 while (true)
@@ -17,44 +18,11 @@ while (true)
         if (sql == "exit")
             break;
 
-        using CamusCommand cmd = connection.CreateSelectCommand(sql);
+        if (sql.Trim().StartsWith("select ", StringComparison.InvariantCultureIgnoreCase))
+            await ExecuteQuery(connection, sql);
+        else
+            await ExecuteNonQuery(connection, builder, sql);
 
-        /*cmd.Parameters.Add("@id", ColumnType.Id, CamusObjectIdGenerator.Generate());
-        cmd.Parameters.Add("@name", ColumnType.String, Guid.NewGuid().ToString()[..20]);
-        cmd.Parameters.Add("@type", ColumnType.String, "mechanical");
-        cmd.Parameters.Add("@year", ColumnType.Integer64, Random.Shared.Next(1900, 2050));*/
-
-        //Assert.Equal(1, await cmd.ExecuteNonQueryAsync());        
-
-        Table? table = null;
-
-        CamusDataReader reader = await cmd.ExecuteReaderAsync();
-
-        while (await reader.ReadAsync())
-        {
-            /*Console.WriteLine(reader.GetString(0));
-            Console.WriteLine(reader.GetString(1));
-            Console.WriteLine(reader.GetString(2));
-            Console.WriteLine(reader.GetInt64(3));*/
-
-            if (table is null)
-            {
-                table = new();
-                table.Border = TableBorder.Square;
-
-                // Add some columns
-                table.AddColumn("Id");
-                table.AddColumn("Name");
-                table.AddColumn("Type");
-                table.AddColumn("Year");
-            }
-
-            // Add some rows
-            table.AddRow(new string[] { reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetInt64(3).ToString() });            
-        }
-
-        if (table is not null)           
-            AnsiConsole.Write(table);
     }
     catch (Exception ex)
     {
@@ -62,10 +30,79 @@ while (true)
     }
 }
 
-static async Task<CamusConnection> GetConnection()
+static async Task ExecuteNonQuery(CamusConnection connection, CamusConnectionStringBuilder builder, string sql)
+{
+    using CamusCommand cmd = new CamusCommand(sql, builder);
+
+    Stopwatch stopwatch = new();
+
+    int affected = await cmd.ExecuteNonQueryAsync();
+
+    Console.WriteLine("Query OK, {0} row affected ({1})", affected, stopwatch.Elapsed);
+}
+
+static async Task ExecuteQuery(CamusConnection connection, string sql)
+{
+    using CamusCommand cmd = connection.CreateSelectCommand(sql);
+
+    /*cmd.Parameters.Add("@id", ColumnType.Id, CamusObjectIdGenerator.Generate());
+    cmd.Parameters.Add("@name", ColumnType.String, Guid.NewGuid().ToString()[..20]);
+    cmd.Parameters.Add("@type", ColumnType.String, "mechanical");
+    cmd.Parameters.Add("@year", ColumnType.Integer64, Random.Shared.Next(1900, 2050));*/
+
+    //Assert.Equal(1, await cmd.ExecuteNonQueryAsync());        
+
+    int rows = 0;
+    Table? table = null;
+    
+    Stopwatch stopwatch = Stopwatch.StartNew();
+
+    CamusDataReader reader = await cmd.ExecuteReaderAsync();
+
+    TimeSpan duration = stopwatch.Elapsed;
+
+    while (await reader.ReadAsync())
+    {
+        Dictionary<string, ColumnValue> current = reader.GetCurrent();
+
+        if (table is null)
+        {
+            table = new();
+            table.Border = TableBorder.Square;
+
+            foreach (KeyValuePair<string, ColumnValue> item in current)
+                table.AddColumn(item.Key);
+        }
+
+        string[] row = new string[current.Count];
+
+        int i = 0;
+
+        foreach (KeyValuePair<string, ColumnValue> item in current)
+        {
+            if (item.Value.Type == ColumnType.Id || item.Value.Type == ColumnType.String)
+                row[i++] = !string.IsNullOrEmpty(item.Value.StrValue) ? item.Value.StrValue!.ToString() : "";
+            else if (item.Value.Type == ColumnType.Integer64)
+                row[i++] = item.Value.LongValue.ToString();
+            else if (item.Value.Type == ColumnType.Bool)
+                row[i++] = item.Value.BoolValue.ToString();
+            else
+                row[i++] = "null";
+        }
+
+        table.AddRow(row);
+        rows++;
+    }
+
+    if (table is not null)
+        AnsiConsole.Write(table);
+
+    Console.WriteLine("{0} rows in set ({1})", rows, duration);
+}
+
+static async Task<(CamusConnection, CamusConnectionStringBuilder)> GetConnection()
 {
     CamusConnection cmConnection;
-
 
     SessionPoolOptions options = new()
     {
@@ -86,5 +123,5 @@ static async Task<CamusConnection> GetConnection()
 
     await cmConnection.OpenAsync();
 
-    return cmConnection;
+    return (cmConnection, builder);
 }
